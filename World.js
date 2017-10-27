b44ad7f2480e6a84a48127cfdcd786b81edb41cf
Ufuueueuru <theultimatewiseguy@gmail.com>
2017-10-26 17:49:01 -0700

    Update World.js

--- World.js
+++ World.js
@@ -1,5 +1,13 @@
 function World() {
   this.run = function() {
-    background(0, 0, 0);
+    background(255, 255, 255);
+    player.draw();
+    for(var i = 0;i < overworld.length;i ++) {
+      for(var u = 0;u < overworld[i].length;u ++) {
+        fill(100, 100, 100);
+        textSize(20);
+        text(overworld[i][u], u * 20, i * 20 + 20);
+      }
+    }
   }
 }
